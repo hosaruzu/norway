8fb35841b4e1b93e65e4338b8098a4b2c85b8080
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-09-16 16:16:20 +0400

    feat: add slider autoplay

--- src/js/components/activity-slider.js
+++ src/js/components/activity-slider.js
@@ -1,9 +1,13 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
 
-Swiper.use([Navigation, Pagination]);
-const swiper = new Swiper('.activities__content', {
+Swiper.use([Navigation, Pagination, Autoplay]);
+new Swiper('.activities__content', {
   slidesPerView: 4,
   spaceBetween: 25,
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+  },
   navigation: {
     nextEl: '.activities__button-next',
     prevEl: '.activities__button-prev',
