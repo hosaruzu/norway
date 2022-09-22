cb5e691c85a7cdb02322d56f9979d9c27165c4b9
Artem Tebenkov <artem.tebenkov@icloud.com>
2022-09-22 11:51:16 +0400

    feat: add adaptive layout

--- src/js/components/date-picker.js
+++ src/js/components/date-picker.js
@@ -1,7 +1,18 @@
 import { Datepicker } from 'vanillajs-datepicker';
 
+const datePickerParent = document.querySelector('.date-picker');
 const datePicker = document.querySelector('input[name="date"]');
+
 const dataPicker = new Datepicker(datePicker, {
   format: 'D, dd M',
   autohide: true,
+});
+
+// events for arrow animation
+datePicker.addEventListener('focus', () => {
+  datePickerParent.classList.add('date-picker--active');
+});
+
+datePicker.addEventListener('focusout', () => {
+  datePickerParent.classList.remove('date-picker--active');
 });
